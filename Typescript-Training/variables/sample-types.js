b4a48fe20a4a485aae8b237c956b283355a95c96
123 <1>
2024-03-15 13:27:09 +0530

    variable demo

---
+++ Typescript-Training/variables/sample-types.js
@@ -0,0 +1,5 @@
+var found = true;
+var firstName = "Nirvan";
+var lastname = "Madapala";
+var grade = 98.0;
+console.log("the grade of " + firstName + " is " + grade);
---
+++ Typescript-Training/variables/sample-types.ts
@@ -0,0 +1,6 @@
+let found: boolean = true;
+let firstName: String = "Nirvan";
+let lastname: String = "Madapala";
+let grade: number = 98.0;
+
+console.log("the grade of " + firstName + " is " + grade);
