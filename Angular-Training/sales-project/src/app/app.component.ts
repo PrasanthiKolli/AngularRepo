ad73025e51f6257daa2f306c228f4bb3bc8c7732
12 <12>
2024-03-21 10:25:39 +0530

    new sales project

--- Angular-Training/sales-project/src/app/app.component.ts
+++ Angular-Training/sales-project/src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'This is my first project';
-  firstName="Nirvan";
-  lastName="Madapala"
+  title = 'sales-project';
 }
---
+++ Angular-Training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  salesPersonLIst:SalesPerson[]=[
+    new SalesPerson("nirvan","madapala","nirvan.madapala@gmail.com",500000),
+    new SalesPerson("giribabu","madapala","giri.madapala@gmail.com",400000),
+    new SalesPerson("prasanthi","kolli","prasanthi.kolli@gmail.com",200000)
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
